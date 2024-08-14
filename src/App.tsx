bb83d015574199e246ce19d399a26173a8f7e08d
haodanhu <141018031+haodanhu@users.noreply.github.com>
2024-08-14 05:16:17 +0000

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	modified:   vite.config.ts 	modified:   yarn.lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-import WebApp from '@twa-dev/sdk'
+import { TonConnectButton } from '@tonconnect/ui-react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,26 +8,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TonConnectButton />
       </div>
-      <h1>TWA + Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
         </button>
       </div>
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
-  base: '/vite-boilerplate/'
-});
+  plugins: [react(),nodePolyfills()],
+  base:'/',
+})
